98d20ac157475af6b0b4722d88d1b84a7e5832db
alain <79479952+chadnpc@users.noreply.github.com>
2025-03-15 20:43:02 +0200

    Update logger.psm1

--- logger.psm1
+++ logger.psm1
@@ -146,7 +146,7 @@ class FileAppender : ILoggerAppender {
 }
 
 # Export types and setup accelerators
-$exportTypes = [Logger], [ILoggerEntry], [LoggingEventType], [ColoredConsoleAppender], [FileAppender]
+$exportTypes = [Logger], [ILoggerEntry], [ColoredConsoleAppender], [LoggingEventType], [ColoredConsoleAppender], [FileAppender]
 $accelerators = [PSObject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
 
 $exportTypes | ForEach-Object {
@@ -156,7 +156,7 @@ $exportTypes | ForEach-Object {
 }
 # Types that will be available to users when they import the module.
 $typestoExport = @(
-  [LogEntry], [LogResource], [Logger]
+  [LogEntry], [FileAppender], [ILoggerEntry], [LogResource], [Logger]
 )
 $TypeAcceleratorsClass = [PsObject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
 foreach ($Type in $typestoExport) {
@@ -178,67 +178,6 @@ $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
     $TypeAcceleratorsClass::Remove($Type.FullName)
   }
 }.GetNewClosure();
-
-
-<# Usage examples (what I envision it will look when completed)
-
->> Installing of the sdk will be done as a module
-
-```powershell
-PS> Install-Module Logger
-```
-
-```powershell
-using module Logger
-
-$logger = New-Object Logger
-$logger.appenders.add([Logger.ColoredConsoleAppender]@{ })
-$logger.appenders.add([Logger.FileAppender]@{ logPath = ('{0}\Logs\miner.log' -f $PSScriptRoot) })
-
-$logger.error('ColoredConsole Appender')
-```
-
-```powershell
-using module Logger
-
-class CustomEntry: Logger.ILoggerEntry
-{
-    static [Logger.ILoggerEntry]yield([String]$text)
-    {
-        return [Logger.ILoggerEntry]@{
-            severity = [Logger.LoggingEventType]::((Get-PSCallStack)[$true].functionName)
-            message = '{1}xxx {0} xxx{1}' -f $text, [Environment]::NewLine
-        }
-    }
-}
-
-$logger = New-Object Logger
-$logger.logEntryType = [CustomEntry]
-$logger.appenders.add([Logger.ColoredConsoleAppender]@{ })
-
-$logger.error('Custom Entry')
-```
-
-```powershell
-using module Logger
-
-class CustomAppender: Logger.ILoggerAppender
-{
-    [void]log([Logger.ILoggerEntry]$entry)
-    {
-        Write-Host $entry.message -ForegroundColor Blue
-    }
-}
-
-$logger = New-Object Logger
-$logger.appenders.add([CustomAppender]@{ })
-
-$logger.error('Custom Appender')
-```
-#>
-
-
-
 
 $scripts = @();
 $Public = Get-ChildItem "$PSScriptRoot/Public" -Filter "*.ps1" -Recurse -ErrorAction SilentlyContinue
