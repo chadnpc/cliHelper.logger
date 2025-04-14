af3e2e19c4281e9a56d7faf60ecd93fdf55d4f30
alain <79479952+chadnpc@users.noreply.github.com>
2025-04-14 16:45:50 +0200

    Create Read-LogEntries.ps1

---
+++ Public/Read-LogEntries.ps1
@@ -0,0 +1,24 @@
+﻿function Read-LogEntries {
+  [CmdletBinding()][OutputType([LogEntry[]] )]
+  param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [Alias('l')][ValidateNotNull()]
+    [Logger]$Logger,
+
+    # LogEntry type
+    [Parameter(Position = 1)]
+    [LogAppenderType]$Type
+  )
+
+  process {
+    $e = switch ($Type) {
+      'CONSOLE' { $Logger.GetConsoleAppender().ReadAllEntries() ; break }
+      'JSON' { $Logger.GetJsonAppender().ReadAllEntries() ; break }
+      'XML' { $Logger.GetXmlAppender().ReadAllEntries() ; break }
+      Default {
+        throw [System.InvalidOperationException]::new("unknown type")
+      }
+    }
+    return $e
+  }
+}
