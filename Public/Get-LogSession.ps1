54140fee05986e8282ccd518cf429b88336364cf
alain <79479952+chadnpc@users.noreply.github.com>
2025-04-21 15:53:31 +0200

    Create Get-LogSession.ps1

---
+++ Public/Get-LogSession.ps1
@@ -0,0 +1,16 @@
+﻿function Get-LogSession {
+  [CmdletBinding()][OutputType([Logsession[]])]
+  param (
+    # session Id
+    [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$Id
+  )
+  process {
+    $sessions = [Logger]::Getallsessions()
+    if ($PSBoundParameters.ContainsKey('Id')) {
+      $sessions = $Id.Contains('*') ? $sessions.Where({ $_.Id -like $Id }) : $sessions.Where({ $_.Id -eq $Id })
+    }
+    return $sessions
+  }
+}
